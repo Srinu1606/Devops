acda1997ce9dcce56b511a8b392ff636b9924533
Srinivas Balivada <srinivasbalivada99@gmail.com>
2025-11-16 09:04:41 +0530

    java code added

--- app.java
+++ app.java
@@ -34,5 +34,17 @@ developer
     }
   }
 }
+public class Main {
 
->>>>>>> Stashed changes
+  public static void main(String[] args) {
+    int rows = 5;
+
+    for (int i = 1; i <= rows; ++i) {
+      for (int j = 1; j <= i; ++j) {
+        System.out.print("* ");
+      }
+      System.out.println();
+    }
+  }
+}
+
